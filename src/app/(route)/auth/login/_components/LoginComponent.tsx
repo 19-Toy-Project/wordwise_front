7beae610535e551c0975e51c89ed7c0465a6e814
leeejin <asd72621@naver.com>
2025-02-18 18:57:41 +0900

    [REFACTOR]:update useSearchParams logic

--- src/app/(route)/auth/login/_components/LoginComponent.tsx
+++ src/app/(route)/auth/login/_components/LoginComponent.tsx
@@ -4,11 +4,13 @@ import useCustomSearchParams from "@/hooks/useCustomSearchParams";
 import { jwtDecode } from "jwt-decode";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+
 export const LoginComponent = () => {
   const { searchParams } = useCustomSearchParams();
   const router = useRouter();
   const { login } = useCookie();
   const code = searchParams?.get("code") || "";
+
   useEffect(() => {
     const handleKaKaoLogin = async () => {
       try {
@@ -21,20 +23,9 @@ export const LoginComponent = () => {
           }
         );
         const data = await response.json();
-
         const accessToken = data.data.accessToken.split(" ")[1];
         const decoded = jwtDecode<{ exp: number }>(accessToken);
-        //setCookie("accessToken", accessToken, decoded?.exp); //15
         login(accessToken, decoded?.exp); //15
-        // await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/auth`, {
-        //   method: "POST",
-        //   headers: { "Content-Type": "application/json" },
-        //   body: JSON.stringify({
-        //     name: "accessToken",
-        //     value: accessToken,
-        //     expires: decoded?.exp,
-        //   }),
-        // });
         const refreshToken = data.data.refreshToken.split(" ")[1];
         const decoded2 = jwtDecode<{ exp: number }>(refreshToken);
 
@@ -47,8 +38,7 @@ export const LoginComponent = () => {
             expires: decoded2?.exp,
             httpOnly: true,
           }),
-        }); //7 * 24 * 60
-
+        });
         router.push("/");
       } catch (error) {
         console.error(error);
@@ -57,5 +47,6 @@ export const LoginComponent = () => {
 
     handleKaKaoLogin();
   }, [code, router]);
+
   return <div>로그인중...</div>;
 };
