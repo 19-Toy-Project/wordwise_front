6be50080cbf8f5080bb25a32cb8cd9e1dca1eba1
leeejin <asd72621@naver.com>
2025-02-18 18:49:41 +0900

    [REFACTOR]:update useSearchParams logic

--- src/app/(route)/auth/login/page.tsx
+++ src/app/(route)/auth/login/page.tsx
@@ -1,65 +1,10 @@
 "use client";
-import { useCookie } from "@/contexts/cookie.context";
-import useCustomSearchParams from "@/hooks/useCustomSearchParams";
-import { jwtDecode } from "jwt-decode";
-import { useRouter } from "next/navigation";
-import { Suspense, useEffect } from "react";
-const LoginComponent = () => {
-  const { login } = useCookie();
-  const { searchParams } = useCustomSearchParams();
-  const router = useRouter();
+import { Suspense } from "react";
+import { LoginComponent } from "./_components/LoginComponent";
 
-  const code = searchParams?.get("code") || "";
-  //
-  useEffect(() => {
-    if (!code) {
-      router.push("/");
-      return;
-    }
-    const handleKaKaoLogin = async () => {
-      try {
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_SERVICE_URL}/api/v1/auth/login`,
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json; charset=UTF-8" },
-            body: JSON.stringify({ code: code }),
-          }
-        );
-        const data = await response.json();
-
-        const accessToken = data.data.accessToken.split(" ")[1];
-        const decoded = jwtDecode<{ exp: number }>(accessToken);
-
-        login(accessToken, decoded?.exp); //15
-
-        const refreshToken = data.data.refreshToken.split(" ")[1];
-        const decoded2 = jwtDecode<{ exp: number }>(refreshToken);
-
-        await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/auth`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            name: "refreshToken",
-            value: refreshToken,
-            expires: decoded2?.exp,
-            httpOnly: true,
-          }),
-        }); //7 * 24 * 60
-
-        router.push("/");
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    handleKaKaoLogin();
-  }, [code, router]);
-  return <div>로그인중...</div>;
-};
 export default function LoginPage() {
   return (
-    <Suspense fallback={<div>로딩 중...</div>}>
+    <Suspense>
       <LoginComponent />
     </Suspense>
   );
