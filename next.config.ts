b2bd071112ffde3366c62f8d61c8b3cb6acff9ca
leeejin <asd72621@naver.com>
2025-02-19 17:12:54 +0900

    [REFACTOR]: cors error

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/v1/:path*", // ✅ trailing slash 제거
-        destination: "http://localhost:8080/api/v1/:path*", // ✅ 정확한 경로 지정
+        destination:
+          "https://port-0-word-wise-dk-m78s70fc7c5822ef.sel4.cloudtype.app/api/v1/:path*", // ✅ 정확한 경로 지정
       },
     ];
   },
