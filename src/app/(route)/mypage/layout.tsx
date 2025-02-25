7a8adc5a378f47119bec5aa2e170ec8ab80d55fd
leeejin <asd72621@naver.com>
2025-02-25 14:57:15 +0900

    [FEAT]: implement calendar

--- src/app/(route)/mypage/layout.tsx
+++ src/app/(route)/mypage/layout.tsx
@@ -1,11 +1,39 @@
 "use client";
 import { Button } from "@/components/buttons";
 import { mypage, mystudy, mywish } from "@/constants/pathname";
+import { useCookie } from "@/contexts/cookie.context";
+import { UserType } from "@/types/type";
+import { fetchWithAuth } from "@/utils/fetchWithAuth";
 import { usePathname } from "next/navigation";
-import { PropsWithChildren } from "react";
-
+import { PropsWithChildren, useEffect, useState } from "react";
+const ranks: Record<string, string> = {
+  DIAMOND: "💎",
+  GOLD: "🥇",
+  SILVER: "🥈",
+  BRONZE: "🥉",
+  IRON: "🌱",
+};
 export default function MyPageLayout({ children }: PropsWithChildren) {
   const pathname = usePathname();
+  const { cookie } = useCookie();
+  const [user, setUser] = useState<UserType | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetchWithAuth(
+        `${process.env.NEXT_PUBLIC_SERVICE_URL}/api/v1/users/profiles`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${cookie}`,
+          },
+        }
+      );
+      const { data: user } = await response.json();
+      setUser(user);
+    };
+    fetchUser();
+  }, [cookie]);
 
   const handleDelete = () => {
     alert("준비중입니다");
@@ -13,6 +41,15 @@ export default function MyPageLayout({ children }: PropsWithChildren) {
   return (
     <div className="wrapper flex flex-col md:flex-row gap-5">
       <nav className="flex flex-col w-full md:w-1/3">
+        <div className="text-center">
+          <h3 className="text-xl font-semibold mb-6">
+            {ranks[user?.userTier ?? "IRON"]}
+          </h3>
+          <p className="text-lg font-semibold text-gray-800">
+            {user?.userName}
+          </p>
+          <p className="text-sm text-gray-500">{user?.userEmail}</p>
+        </div>
         <Button
           href={mypage}
           className={`hover:bg-background hover:font-bold text-center p-3 ${
--- src/app/(route)/mypage/page.tsx
+++ src/app/(route)/mypage/page.tsx
@@ -1,32 +1,3 @@
-import { fetchWithAuth } from "@/utils/fetchWithAuth";
-import { cookies } from "next/headers";
-const ranks: Record<string, string> = {
-  DIAMOND: "💎",
-  GOLD: "🥇",
-  SILVER: "🥈",
-  BRONZE: "🥉",
-  IRON: "🌱",
-};
 export default async function UserPage() {
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get("accessToken")?.value;
-
-  const response = await fetchWithAuth(
-    `${process.env.NEXT_PUBLIC_SERVICE_URL}/api/v1/users/profiles`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    }
-  );
-  const { data: user } = await response.json();
-
-  return (
-    <div className="text-center">
-      <h3 className="text-xl font-semibold mb-6">{ranks[user?.userTier]}</h3>
-      <p className="text-lg font-semibold text-gray-800">{user?.userName}</p>
-      <p className="text-sm text-gray-500">{user?.userEmail}</p>
-    </div>
-  );
+  return <div className="text-center"></div>;
 }
