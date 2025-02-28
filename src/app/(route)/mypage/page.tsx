30402e46e70734b97943ee9a6481d51785f52c6d
leeejin <asd72621@naver.com>
2025-02-28 16:22:17 +0900

    [FEAT]: connect calendar, studiedCount with backend

--- src/app/(route)/mypage/page.tsx
+++ src/app/(route)/mypage/page.tsx
@@ -1,3 +1,28 @@
-export default function UserPage() {
-  return <div className="text-center"></div>;
+import Calender from "@/components/calender/calender";
+import { fetchWithAuth } from "@/utils/fetchWithAuth";
+import { cookies } from "next/headers";
+
+export default async function UserPage() {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value || null;
+  const response = await fetchWithAuth(
+    `${process.env.NEXT_PUBLIC_SERVICE_URL}/api/v2/users/learned-sentences/count`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+  const { data: studiedSentence } = await response.json();
+
+  return (
+    <div>
+      <div className="border border-background rounded-md p-5">
+        <p className="font-bold">공부한 문장 개수</p>
+        <p>{studiedSentence.studySentenceCount}개</p>
+      </div>
+      <Calender />
+    </div>
+  );
 }
--- src/components/calender/calender.tsx
+++ src/components/calender/calender.tsx
@@ -1,52 +1,85 @@
 "use client";
+import { useCookie } from "@/contexts/cookie.context";
 import useCalendar from "@/hooks/useCalendar";
+import { fetchWithAuth } from "@/utils/fetchWithAuth";
 import { subMonths } from "date-fns";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { IconButton } from "../buttons";
 
-export default function Calender() {
+export default function Calendar() {
   const calendar = useCalendar();
-  const [select, setSelect] = useState<number[]>([]);
-  
+  const { cookie } = useCookie();
+  const [schedules, setSchedules] = useState<string[]>([]);
+
+  // 오늘 날짜
+  const today = new Date();
+  const todayFormatted = `${today.getFullYear()}-${String(
+    today.getMonth() + 1
+  ).padStart(2, "0")}-${String(today.getDate()).padStart(2, "0")}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetchWithAuth(
+        `${
+          process.env.NEXT_PUBLIC_SERVICE_URL
+        }/api/v2/users/calendar?year=${calendar.currentDate.getFullYear()}&month=${
+          calendar.currentDate.getMonth() + 1
+        }`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${cookie}`,
+          },
+        }
+      );
+      const { data } = await response.json();
+      setSchedules(data);
+    };
+    fetchData();
+  }, [calendar.currentDate, cookie]);
+
   return (
-    <div>
-      <button
-        onClick={() => {
-          calendar.setCurrentDate(subMonths(calendar.currentDate, 1));
-        }}
-      >
-        &lt;
-      </button>
-      <strong>{calendar.currentDate.getMonth() + 1}</strong>
-      <button
-        onClick={() => {
-          calendar.setCurrentDate(subMonths(calendar.currentDate, -1));
-        }}
-      >
-        &gt;
-      </button>
+    <div className="text-center">
+      <IconButton
+        onClick={() =>
+          calendar.setCurrentDate(subMonths(calendar.currentDate, 1))
+        }
+        icon={() => <FaAngleLeft color="black" size={20} />}
+      />
+      <strong>
+        {calendar.currentDate.getFullYear()}년{" "}
+        {calendar.currentDate.getMonth() + 1}월
+      </strong>
+      <IconButton
+        onClick={() =>
+          calendar.setCurrentDate(subMonths(calendar.currentDate, -1))
+        }
+        icon={() => <FaAngleRight color="black" size={20} />}
+      />
       <div>
-        {calendar.weekCalendarList.map((item) => (
-          <div className="flex px-20 w-full" key={Math.random()}>
-            {item.map((day) => (
-              <button
-                onClick={() => {
-                  const findItem = select.find((item) => item === day);
-                  if (findItem) {
-                    setSelect((prev) => prev.filter((item) => item !== day));
-                  } else {
-                    setSelect((prev) => [...prev, day]);
-                  }
-                }}
-                className={`flex justify-between min-w-[calc(100%/7)] active:bg-blue-500 hover:bg-blue-400 items-center text-center ${
-                  day === 0 ? " invisible" : ""
-                }
-                ${select.find((item) => item === day) ? " bg-blue-600" : ""}
-                `}
-                key={Math.random()}
-              >
-                {day}
-              </button>
-            ))}
+        {calendar.weekCalendarList.map((week, weekIndex) => (
+          <div className="flex px-20 w-full" key={weekIndex}>
+            {week.map(({ day, isCurrentMonth }, dayIndex) => {
+              const formattedDate = `${calendar.currentDate.getFullYear()}-${String(
+                calendar.currentDate.getMonth() + 1
+              ).padStart(2, "0")}-${String(day).padStart(2, "0")}`;
+
+              const isScheduled = schedules.includes(formattedDate);
+              const isToday = formattedDate === todayFormatted; // 오늘 날짜 확인
+
+              return (
+                <button
+                  key={`${weekIndex}-${dayIndex}`}
+                  className={`flex justify-center min-w-[calc(100%/7)] items-center text-center p-2 rounded-md transition-colors duration-300 
+                    ${isCurrentMonth ? "" : "invisible"} 
+                    ${isScheduled ? "bg-background" : "bg-white"} 
+                    ${isToday ? "text-mainColor font-bold" : ""}`} // 오늘 날짜면 테두리 추가
+                >
+                  {day}
+                </button>
+              );
+            })}
           </div>
         ))}
       </div>
