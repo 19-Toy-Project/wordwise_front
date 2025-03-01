6cb592a7f89281d7887b2fa62ed0201859d9751b
leeejin <asd72621@naver.com>
2025-03-01 15:51:35 +0900

    [REFACOTR]: check ssr

--- src/app/(route)/mypage/page.tsx
+++ src/app/(route)/mypage/page.tsx
@@ -1,27 +1,32 @@
 import Calender from "@/components/calender/calender";
+import { useCookie } from "@/contexts/cookie.context";
 import { fetchWithAuth } from "@/utils/fetchWithAuth";
-import { cookies } from "next/headers";
+import { useEffect, useState } from "react";
 
-export default async function UserPage() {
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get("accessToken")?.value || null;
-
-  const response = await fetchWithAuth(
-    `${process.env.NEXT_PUBLIC_SERVICE_URL}/api/v2/users/learned-sentences/count`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    }
-  );
-  const { data: studiedSentence } = await response.json();
-
+export default function UserPage() {
+  const { cookie } = useCookie();
+  const [studied, setStudied] = useState<number>(0);
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetchWithAuth(
+        `${process.env.NEXT_PUBLIC_SERVICE_URL}/api/v2/users/learned-sentences/count`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${cookie}`,
+          },
+        }
+      );
+      const { data: studiedSentence } = await response.json();
+      setStudied(studiedSentence.studySentenceCount);
+    };
+    fetchUser();
+  }, [cookie]);
   return (
     <div>
       <div className="border border-background rounded-md p-5">
         <p className="font-bold">공부한 문장 개수</p>
-        <p>{studiedSentence.studySentenceCount}개</p>
+        <p>{studied}개</p>
       </div>
       <Calender />
     </div>
